5ed7246caa659ce5c61e768da59fcf1ec89cbd6c
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-11-06 21:53:39 -0200

    updt

--- config/nlp.js
+++ config/nlp.js
@@ -1,5 +1,7 @@
 const nlp = {};
 
+console.log('==============> APIAI_TOKEN: ', process.env.NLP_APIAI_TOKEN.substring(1, 4));
+
 nlp.apiai = { "token": process.env.NLP_APIAI_TOKEN };
     
 module.exports = nlp;
