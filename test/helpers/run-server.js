71762f676adb977878a24960658137f6ed991b18
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-11-08 02:16:17 -0200

    updt

--- test/helpers/run-server.js
+++ test/helpers/run-server.js
@@ -1,5 +1,9 @@
-const env = require('node-env-file');
-env(__dirname + '/.env');
+const fs = require('fs');
+
+if (fs.existsSync(__dirname + '/.env')) {
+  const env = require('node-env-file');
+  env(__dirname + '/.env');
+}
 
 const TestAppServer = require(__dirname + '/test-app-server');
 const testApp = new TestAppServer();
