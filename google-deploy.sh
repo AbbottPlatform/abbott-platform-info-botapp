ef938805f1e34c3d5bccf20241fc7786bd46ac4d
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-11-09 17:11:46 -0200

    updt

--- google-deploy.sh
+++ google-deploy.sh
@@ -3,9 +3,5 @@
 # Authenticate with the Google Services
 codeship_google authenticate
 
-# switch to the directory containing your app.yml (or similar) configuration file
-# note that your repository is mounted as a volume to the /deploy directory
-cd /deploy/
-
 # deploy the application
 gcloud app deploy --project ciandt-cognitive-sandbox --version=beta-ci-travis --no-stop-previous-version
