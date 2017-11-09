6077fffa286b3140a5c50add0d1eb2856fc1a675
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-11-09 17:42:51 -0200

    updt

--- google-deploy.sh
+++ google-deploy.sh
@@ -5,5 +5,7 @@ codeship_google authenticate
 
 cd /deploy/
 
+rm -f Dockerfile
+
 # deploy the application
 gcloud app deploy --project ciandt-cognitive-sandbox --version=beta-ci-travis --no-stop-previous-version --quiet
